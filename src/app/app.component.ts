21e3aa4e4346cfdba91002cc876ce6dac4f20414
aliyurekli32 <adamjesperblack@gmail.com>
2023-03-23 14:18:47 +0300

    start: first look to angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-first-app';
+  title = 'Angular-First App';
+  name='My first App Name'
 }
